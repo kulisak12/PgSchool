48725d212ffa145d720cf407b7141167a2e45f68
kulisak12 <kulisak12lp@gmail.com>
2018-09-13 21:24:13 +0200

    Initial commit

---
+++ Java/Prvni.java
@@ -0,0 +1,8 @@
+public class Prvni {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.print("Hello world again"); // no newline
+    System.out.println("I say \"Hello\", ok?"); // escaping
+    System.out.println("More\nfancy\tstuff\\yay"); // other escape sequences
+  }
+}
---
+++ Java/Ukol.java
@@ -0,0 +1,10 @@
+public class Ukol {
+  public static void main(String[] args) {
+    System.out.println("._____");         // ._____
+    System.out.println("|\\    \\");      // |\    \
+    System.out.println("| \\ \\\\ \\");   // | \ \\ \
+    System.out.println("|  \\    \\");    // |  \    \
+    System.out.println("|   \"\"\"\"\""); // |   """""
+    System.out.println("`\"\"\"\"\"");    // `"""""
+  }
+}
