0550bff55081755508707ae6ee038eaa8302e61d
kulisak12 <31045616+kulisak12@users.noreply.github.com>
2018-12-04 19:15:14 +0100

    Extended Mnohoclen
    Now supports subtraction and multiplication.
    Also refactored addition a bit.

--- Lekce10/Mnohoclen.java
+++ Lekce10/Mnohoclen.java
@@ -25,16 +25,54 @@ public class Mnohoclen {
     }
 
     public void secti(Mnohoclen m) {
-        int vysledek[] = new int[max(this.ziskejStupen(), m.ziskejStupen()) + 1];
-        for (int i = 0; i < vysledek.length; i++) {
+        // vytvor pole pro uchovani vysledku
+        int pocetClenuVysledku = max(this.ziskejStupen(), m.ziskejStupen()) + 1;
+        int vysledek[] = new int[pocetClenuVysledku];
+        for (int i = 0; i < pocetClenuVysledku; i++) {
             vysledek[i] = 0;
         }
-
+        
+        // pricti oba mnohocleny
         for (int i = 0; i <= this.ziskejStupen(); i++) {
-            vysledek[i] += this.koeficienty[i];
+            vysledek[i] += this.ziskejKoeficient(i);
         }
         for (int i = 0; i <= m.ziskejStupen(); i++) {
             vysledek[i] += m.ziskejKoeficient(i);
+        }
+        koeficienty = vysledek;
+    }
+
+    public void odecti(Mnohoclen m) {
+        // vytvor pole pro uchovani vysledku
+        int pocetClenuVysledku = max(this.ziskejStupen(), m.ziskejStupen()) + 1;
+        int vysledek[] = new int[pocetClenuVysledku];
+        for (int i = 0; i < pocetClenuVysledku; i++) {
+            vysledek[i] = 0;
+        }
+        
+        // pricti prvni, odecti druhy mnohoclen
+        for (int i = 0; i <= this.ziskejStupen(); i++) {
+            vysledek[i] += this.ziskejKoeficient(i);
+        }
+        for (int i = 0; i <= m.ziskejStupen(); i++) {
+            vysledek[i] -= m.ziskejKoeficient(i);
+        }
+        koeficienty = vysledek;
+    }
+
+    public void vynasob(Mnohoclen m) {
+        // vytvor pole pro uchovani vysledku
+        int pocetClenuVysledku = this.ziskejStupen() * m.ziskejStupen + 1;
+        int vysledek[] = new int[pocetClenuVysledku];
+        for (int i = 0; i < pocetClenuVysledku; i++) {
+            vysledek[i] = 0;
+        }
+        
+        // kazdy clen prvniho mnohoclenu vynasob celym druhym mnohoclenem
+        for (int i = 0; i <= this.ziskejStupen(); i++) {
+            for (int j = 0; j <= m.ziskejStupen(); j++) {
+                vysledek[i + j] += this.ziskejKoeficient(i) + m.ziskejKoeficient(j);
+            }
         }
         koeficienty = vysledek;
     }
