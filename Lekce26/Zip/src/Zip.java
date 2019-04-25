873c59cab93a549755d7f4fdf7c7acf5b01f93fd
kulisak12 <kulisak12lp@gmail.com>
2019-04-25 16:00:15 +0200

    Improved Zip
    Added basic code for file printing
    Just the outline

--- Lekce26/Zip/src/Zip.java
+++ Lekce26/Zip/src/Zip.java
@@ -1,52 +1,43 @@
-
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class Zip {
 
-	public static void main(String[] args) {
-		Actions actions = new Actions();
-		int state = 0; // 0 = argument, 1 = dir path
-		for (String arg : args) {
-			state = parseAction(actions, arg, state);
-		}
-		// TODO state == 1
+	public static void main(String[] args) throws IOException {
+		ZipFile zipFile = new ZipFile(args[0]);
 		
+		int argIndex = 1;
+		if (args[argIndex].equals("-t")) {
+			printZipContent(zipFile, false);
+			argIndex++;
+			if (argIndex >= args.length) {
+				return;
+			}
+			if (args[argIndex].equals("-l")) {
+				printZipContent(zipFile, true);
+				argIndex++;
+				if (argIndex >= args.length) {
+					return;
+				}
+			}
+		}
 		
 	}
 	
-	public static int parseAction(Actions actionsList, String arg, int state) {
-		if (state == 0) {
-			if (arg == "-t") {
-				actionsList.shouldPrintDir = true;
-				return 0;
-			}
-			if (arg == "-l") {
-				actionsList.shouldIncludeFileInfo = true;
-				return 0;
-			}
-			if (arg == "-x") {
-				actionsList.shouldUnzip = true;
-				// TODO get current dir
-				return 0;
-			}
-			if (arg == "-D") {
-				actionsList.shouldPrintDir = true;
-				return 0;
-			}
-			if (arg == "-d") {
-				return 1;
-			}
-		}
-		else { // state == 1
-			actionsList.unzipDir = arg;
-			return 0;
+	public static void printZipContent(ZipFile zipFile, boolean printAdditionalData) {
+		Enumeration<? extends ZipEntry> content = zipFile.entries();
+		while (content.hasMoreElements()) {
+			printFileInfo(content.nextElement(), printAdditionalData);
 		}
 	}
 	
-	private static class Actions {
-		boolean shouldPrintDir = false;
-		boolean shouldIncludeFileInfo = false;
-		boolean shouldUnzip = false;
-		String unzipDir;
+	public static void printFileInfo(ZipEntry file, boolean printAdditionalData) {
+		System.out.print(file.getName());
+		if (printAdditionalData) {
+			System.out.printf(" %d", file.getTime());
+		}
+		System.out.println();
 	}
-
 }
