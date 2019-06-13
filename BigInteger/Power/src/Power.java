0d8b0eddb22e206fbffcc3c5f0bd11aa3a28bb89
kulisak12 <kulisak12lp@gmail.com>
2019-06-13 19:23:00 +0200

    Fixed BigInteger

--- BigInteger/Power/src/Power.java
+++ BigInteger/Power/src/Power.java
@@ -2,21 +2,21 @@ import java.math.BigInteger;
 
 public class Power {
 	public static void main(String[] args) {
-		BigInteger largeNum = power(BigInteger.valueOf(3), 2);
+		int base = Integer.parseInt(args[0]);
+		int exp = Integer.parseInt(args[1]);
+		BigInteger largeNum = power(BigInteger.valueOf(base), exp);
 		System.out.print(largeNum.toString());
 	}
 	
 	public static BigInteger power(BigInteger base, int exp) {
-		BigInteger result = base;
-		
-		while (exp > 1) {
-			result = result.multiply(result);
-			exp /= 2;
-			
+		BigInteger result = BigInteger.ONE;
+		while (exp > 0) {
 			if (exp % 2 == 1) {
 				result = result.multiply(base);
 			}
 			
+			base = base.multiply(base);
+			exp /= 2;
 		}
 		
 		return result;
